0d2385f4b4dce3c90b20a11f18e349543db5a301
Tony Nguyen <tonywins35@gmail.com>
2025-07-01 17:30:44 -0400

    Initial Commit

---
+++ PopSign/PopSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopSignApp.swift
+//  PopSign
+//
+//  Created by Tony Nguyen on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PopSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PopSignTests/PopSignTests.swift
@@ -0,0 +1,17 @@
+//
+//  PopSignTests.swift
+//  PopSignTests
+//
+//  Created by Tony Nguyen on 7/1/25.
+//
+
+import Testing
+@testable import PopSign
+
+struct PopSignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
